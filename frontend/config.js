8274b69e4670b35421248812eab7076191a1b4cc
ahmad18697 <ahmadraza993432@gmail.com>
2025-06-07 22:34:44 +0530

    db url change from local to prod

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
-// const BACKEND_URL = 'http://localhost:5000';
+const BACKEND_URL = 'http://localhost:5000';
 
-const BACKEND_URL = 'https://textsummarizer-si3j.onrender.com';
+// const BACKEND_URL = 'https://textsummarizer-si3j.onrender.com';
 
 export default BACKEND_URL;
